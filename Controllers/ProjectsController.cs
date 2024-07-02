d4be529eb60a6a890376e12fe251ea21feec5443
Irina Sviridova <sviridovairis@gmail.com>
2024-07-02 17:57:30 +0300

    add projects page

---
+++ Controllers/ProjectsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CRMSystem.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task GetProjectDescription()
+        {
+            Response.ContentType="text/html";
+            await Response.WriteAsync("Project descriprion here");
+        }
+    }
+}
